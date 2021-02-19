d036207a83c9f06ce10dcb354371d1711e25b540
pn88 <piotrnoga88@gmail.com>
2021-02-19 20:10:18 +0100

    add MD5

---
+++ src/MD5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5 {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        scan.close();
+
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(str.getBytes());
+        byte[] digest = md.digest();
+
+        for (byte b : digest) {
+            System.out.format("%02x", b);
+        }
+    }
+}
